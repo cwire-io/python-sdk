400d4c2fea38471fe9ecd6287c40d07a847b4175
leon <leonlowitzki@gmail.com>
2020-09-22 21:51:52 +0200

    add constants: error codes

---
+++ src/constants.py
@@ -0,0 +1,9 @@
+ERROR_CODES = {
+    409: "Conflict",
+    400: "Bad Request",
+    401: "Unauthorized",
+    404: "Page not found :(",
+    422: "Wrong body entities",
+    500: "Internal server error",
+    503: "Service Unavailable"
+}
